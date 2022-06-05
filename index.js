a3b6d7b9b158ee26f2ea2257189a163d44059f3f
Psianturi <posma2018@gmail.com>
2022-06-05 20:08:46 +0700

    Update challenge code

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data, callback) => {
+  const listName = callback(data);
+	const result = []
+	for (let i = 0; i < listName.length; i++) {
+		result.push(`${i + 1}. ${listName[i]}`)
+	}
+	return result
+};
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+  return data.sort((a, b) => a.localeCompare(b))
+}
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+  return data.sort((a, b) => b.localeCompare(a))
+}
+
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
